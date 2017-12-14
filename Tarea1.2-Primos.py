b1a89823744e62b28a9cf248e6394065c838e611
Marco A. Disner <34427543+mdisner@users.noreply.github.com>
2017-12-14 00:13:22 -0700

    Update Tarea1.2-Primos.py

--- Tarea1.2-Primos.py
+++ Tarea1.2-Primos.py
@@ -18,5 +18,14 @@ def primos():
        print "FALSE"
        print "%s no es numero primo" % n
        break
+       
+"""
+Esta funcion recibe un numero entero como valor, despues itera cada numero del rango (2,n) calculando el modulo respecto a cada 
+numero anterior a este, si el modulo es distinto de cero, entonces a la variable j se le suma 1. 
+Si al final de la iteracion resulta que cada modulo fue distinto de cero se tendra que j es igual a x, en otras palabras, 
+x sera entonces un numero primo, por lo tanto se imprime un TRUE y se confirma que x es un numero primo.
+En caso contrario, si hay un numero tal que el modulo sea igual a cero, automaticamente se descarta dicho numero y se imprime
+FALSE.
+"""        
 
 primos()   
