a36c10ac1a2c63c8e7f3b4baf33cbea4b9444620
Marco A. Disner <34427543+mdisner@users.noreply.github.com>
2017-12-13 23:17:49 -0700

    Update Tarea1.3-Primos.py

--- Tarea1.3-Primos.py
+++ Tarea1.3-Primos.py
@@ -7,6 +7,16 @@ lista = [2]
 
 def primos2():
  print lista[:n]
+ 
+"""
+
+Esta funcion se encarga de imprimir la cantidad de elementos n, solicitada por el usuario de la lista que contiene solo numeros primos resultantes de la funcion primos
+
+"""
+
+ 
+ 
+ 
 
 def primos():
    for x in range(2,10000): 
@@ -20,6 +30,13 @@ def primos():
      else:
        break
    primos2()
+   
+"""
+Esta funcion itera cada numero del rango (2,10000) calculando el modulo respecto a cada numero anterior a este, si el modulo es distinto de cero, entonces a la variable j se le suma 1, si al final de la iteracion resulta que cada modulo fue distinto de cero se tendra que j es igual a x, en otras palabras, x es entonces un numero primo, por lo tanto se agrega a la lista llamada lista.
+En caso contrario, si hay un numero tal que el modulo sea igual a cero, automaticamente se descarta dicho numero para entrar a la lista.
+
+"""
+      
 primos()
 
 
